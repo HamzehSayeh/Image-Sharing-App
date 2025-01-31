de24eddca3049d94aa4f7a4b511ef5db5c5abc24
Hamzeh Sayeh <hamzehsayeh6@gmail.com>
2025-01-31 18:44:43 +0200

    Update Post.js

--- image_sharing_app/api/models/Post.js
+++ image_sharing_app/api/models/Post.js
@@ -1,1 +1,22 @@
-// Create Post scheme here
+const mongoose = require("mongoose");
+const photo = require("./photo");
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      max: 500,
+    },
+    imageProprties: {
+      type: String,
+      required: true,
+    },
+  },
+  { Timestamp: true }
+);
+
+module.exports = mongoose.model("Post", postSchema);
